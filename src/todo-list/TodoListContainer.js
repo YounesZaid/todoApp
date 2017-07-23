7a6d7826d6ea67b058245d238f99af88b090cf6d
younes <zaidyounes94@gmail.com>
2017-07-23 19:09:19 +0100

    Added dynamic todo items

--- src/todo-list/TodoListContainer.js
+++ src/todo-list/TodoListContainer.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './TodoListContainer.css';
 
 export default class TodoListContainer extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'myTodo'
+        },
+        {
+          id: 2,
+          text: 'myTodo2'
+        },
+        {
+          id: 3,
+          text: 'myTodo3'
+        },
+        {
+          id: 4,
+          text: 'myTodo4'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="todo-list-container">
@@ -10,10 +34,14 @@ export default class TodoListContainer extends Component {
         </div>
         }
         <h3>My todos:</h3>
-        <p className="todo-item">
-          <span>My todo item</span>
-          <a href="#" className="zmdi zmdi-delete"></a>
-        </p>
+        {this.state.todos.map(todoObj => {
+          return (
+            <p key={todoObj.id} className="todo-item">
+              <span>{todoObj.text}</span>
+              <a href="#" className="zmdi zmdi-delete"></a>
+            </p>
+          );
+        })}
       </div>
     )
   }
