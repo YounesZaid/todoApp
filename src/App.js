253c78e51eceaf24e02183e07c75ecc847f57f06
younes <zaidyounes94@gmail.com>
2017-07-26 23:14:38 +0100

    Added Filter and Lodash examples for delete item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import lodash from 'lodash';
 
 import LogoContainer from './logo/LogoContainer';
 import TodoListContainer from './todo-list/TodoListContainer';
@@ -31,9 +32,35 @@ export default class App extends Component {
     super(props, context);
     this._deleteItem = this._deleteItem.bind(this);
     this._addItem = this._addItem.bind(this);
+    // lodash
+    this._deleteItemLodash = this._deleteItemLodash.bind(this);
+    this._addItemLodash = this._addItemLodash.bind(this);
+    // filter
+    this._deleteItemFilter = this._deleteItemFilter.bind(this);
     this.state = {
       todos: _defaultTodos
     }
+  }
+
+  // Lodash
+  _deleteItemLodash(todoObj) {
+    let newArray = lodash.cloneDeep(this.state.todos);
+    lodash.remove(newArray, (element) => {
+      return element.id === todoObj.id;
+    });
+    this.setState({
+      todos: newArray
+    });
+  }
+  
+  // Filter
+  _deleteItemFilter(todoObj) {
+    let newArray = this.state.todos.filter(element => {
+      return element.id !== todoObj.id;
+    });
+    this.setState({
+      todos: newArray
+    })
   }
 
   _deleteItem(todoObj) {
@@ -44,6 +71,19 @@ export default class App extends Component {
         newArray.push(element);
       }
     })
+    this.setState({
+      todos: newArray
+    })
+  }
+
+  _addItemLodash(text) {
+    let newArray = lodash.cloneDeep(this.state.todos);
+    let todoItem = {
+      id: Math.round(Math.random() * 100),
+      text: text
+    };
+
+    newArray.push(todoItem);
     this.setState({
       todos: newArray
     })
@@ -69,7 +109,7 @@ export default class App extends Component {
       <div className="container">
         <LogoContainer />
         <div className="sub-container">
-          <TodoListContainer todosList={this.state.todos} deleteTodoItem={this._deleteItem} />
+          <TodoListContainer todosList={this.state.todos} deleteTodoItem={this._deleteItemFilter} />
           <AddTodoContainer addTodoItem={this._addItem} />
         </div>
       </div>
